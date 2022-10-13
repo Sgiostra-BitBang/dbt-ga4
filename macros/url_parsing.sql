db6946cfd4d27a50d4dcb45c966b14bdecfc07bc
dgitis <damon@caretjuice.com>
2022-10-13 13:15:56 -0700

    trim trailing slash base commit

--- macros/url_parsing.sql
+++ macros/url_parsing.sql
@@ -9,3 +9,14 @@
 {% macro remove_query_parameters(url, parameters)%}
 REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE({{url}}, '(\\?|&)({{ parameters|join("|") }})=[^&]*', '\\1'), '\\?&+', '?'), '&+', '&'), '\\?$|&$', '')
 {% endmacro %}
+
+
+{% macro trim_trailing_slash(url, delimiter) %}
+    {% if delimiter is none %}
+        {% return url|trim('/') %}
+    {% else %}
+        {% set pl_list = url|split(delimiter,1) %}
+        {% set pl_list[0] = pl_list[0]|trim('/') %}
+        {% return pl_list|join() %}
+    {% endif %}
+{% endmacro %}
